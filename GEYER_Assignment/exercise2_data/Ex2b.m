6918a8a57855fec509db107ee0d69c866d818424
GaiaCarparelli <gaia.carparelli@epfl.ch>
2019-05-25 15:07:07 +0200

    dfghjk

--- GEYER_Assignment/exercise2_data/Ex2b.m
+++ GEYER_Assignment/exercise2_data/Ex2b.m
@@ -5,10 +5,27 @@
 clc;
 clear all;
 close all;
-%% Run simulation
 
-init();
-
+%% RUN
+init()
+%% Run simulation- TA symmetric - feedback
 % After running init simulation
-struct_mRFX = mRFX;
-%save('struct_mRFX.mat','struct_mRFX'); % to use it later
+struct_mRFX_TA_symetric_feedback = mRFX;
+save('struct_mRFX_TA_symetric_feedback.mat','struct_mRFX_TA_symetric_feedback'); % to use it later
+%0.024
+%% Run simulation- TA asymmetric - feedback
+% After running init simulation
+struct_mRFX_TA_asymetric_feedback = mRFX;
+save('struct_mRFX_TA_asymetric_feedback.mat','struct_mRFX_TA_asymetric_feedback'); % to use it later
+% 0.049
+%% Run simulation- TA symmetric - stim
+% After running init simulation
+struct_mRFX_TA_symetric_stim = mRFX;
+save('struct_mRFX_TA_symetric_stim.mat','struct_mRFX_TA_symetric_stim'); % to use it later
+% 0.375
+%% Run simulation- TA asymmetric - stim
+% After running init simulation
+struct_mRFX_TA_asymetric_stim = mRFX;
+save('struct_mRFX_TA_asymetric_stim.mat','struct_mRFX_TA_asymetric_stim'); % to use it later
+% 0.59
+
